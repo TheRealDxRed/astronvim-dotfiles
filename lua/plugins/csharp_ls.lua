4f35bc644916a6028c5d9dcebeb974d915aa3a67
DxRed <ben.dumouchel@gmail.com>
2025-08-08 11:53:08 -0500

    Make csharp_ls.lua a valid AstroNvim plugin spec for lazy.nvim

--- lua/plugins/csharp_ls.lua
+++ lua/plugins/csharp_ls.lua
@@ -1,13 +1,20 @@
--- Override for csharp_ls under astrocommunity.pack.cs
-local csharp_ls = require('lspconfig').csharp_ls
-
-csharp_ls.setup {
-    on_attach = function(client)
-        client.config.settings = client.config.settings or {}
-        client.config.settings.editor = {
-            tabSize = 2,
-            indentSize = 2,
-            insertSpaces = false,
-        }
-    end,
+---@type LazySpec
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        csharp_ls = {
+          on_attach = function(client)
+            client.config.settings = client.config.settings or {}
+            client.config.settings.editor = {
+              tabSize = 2,
+              indentSize = 2,
+              insertSpaces = false,
+            }
+          end,
+        },
+      },
+    },
+  },
 }
