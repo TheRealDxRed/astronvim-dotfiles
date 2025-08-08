d81c682a76ab8774662a7e9009dca09d738e4ef2
DxRed <ben.dumouchel@gmail.com>
2025-08-08 11:58:10 -0500

    Update csharp_ls.lua with new LSP settings

--- lua/plugins/csharp_ls.lua
+++ lua/plugins/csharp_ls.lua
@@ -5,14 +5,13 @@ return {
     opts = {
       servers = {
         csharp_ls = {
-          on_attach = function(client)
-            client.config.settings = client.config.settings or {}
-            client.config.settings.editor = {
+          settings = {
+            editor = {
               tabSize = 2,
               indentSize = 2,
               insertSpaces = false,
-            }
-          end,
+            },
+          },
         },
       },
     },
