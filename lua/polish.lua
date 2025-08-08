c69cf8efabeb3d9ced8b81aebffdbfac0c88c57d
TheRealDxRed <ben.dumouchel@gmail.com>
2025-08-08 13:50:50 -0400

    re-enable csharpier. unfuck that guy ig

--- lua/polish.lua
+++ lua/polish.lua
@@ -44,7 +44,7 @@ end
 vim.o.cmdheight = 1
 
 -- fucking csharpier doesn't work so disable it or i will RIOT
-require("null-ls").disable "csharpier"
+-- require("null-ls").disable "csharpier"
 
 -- Setup csharp_ls indentation settings
 local lspconfig = require "lspconfig"
@@ -61,11 +61,11 @@ lspconfig.csharp_ls.setup {
 -- Override csharpier formatter to use correct arguments so formatting-on-save works
 local null_ls_status, null_ls = pcall(require, "null-ls")
 if null_ls_status then
-  null_ls.register(null_ls.builtins.formatting.csharpier.with({
+  null_ls.register(null_ls.builtins.formatting.csharpier.with {
     command = "dotnet-csharpier",
     args = { "--write-stdout", "--no-cache" },
     filetypes = { "cs" },
-  }))
+  })
 end
 
 -- Set buffer-local indentation for C# files (Neovim style)
