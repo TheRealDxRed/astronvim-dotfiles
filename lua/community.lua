9b97a733e6bd8a33d8cb9a18fa22e3bdd7721810
DxRed <ben.dumouchel@gmail.com>
2025-08-08 11:45:44 -0500

    Update lua/community.lua with new content

--- lua/community.lua
+++ lua/community.lua
@@ -1,15 +1,26 @@
--- Override for csharp_ls under astrocommunity.pack.cs
-local csharp_ls = require('lspconfig').csharp_ls
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
 
-csharp_ls.setup {
-    on_attach = function(client)
-        client.config.settings = client.config.settings or {}
-        client.config.settings.editor = {
-            tabSize = 2,
-            indentSize = 2,
-            insertSpaces = false,
-        }
-    end,
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.pack.cs-omnisharp" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.godot" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.recipes.ai" },
+  { import = "astrocommunity.recipes.neovide" },
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+  -- import/override with your plugins folder
 }
-
-return csharp_ls;
