7ca2bc940fe5f826e68887970de6f38af883fb6d
TheRealDxRed <ben.dumouchel@gmail.com>
2026-02-07 23:54:44 -0500

    adjust font size + neovide update

--- init.lua
+++ init.lua
@@ -1,10 +1,21 @@
 if vim.g.neovide then
-  vim.o.guifont = "CaskaydiaCove Nerd Font:h14"
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h12"
   vim.g.neovide_floating_shadow = true
   vim.g.neovide_floating_z_height = 10
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
-  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_opacity = 0.9
+end
+
+if vim.fn.has "windows" then
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellcmdflag = "-ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding"
+    .. "=[System.Text.Encoding]::UTF8;$PSStyle.Formatting.Error = '';$PSStyle.Formatting.ErrorAccent = '';"
+    .. "$PSStyle.Formatting.Warning = '';$PSStyle.OutputRendering = 'PlainText';"
+  vim.opt.shellredir = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
 end
 
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
